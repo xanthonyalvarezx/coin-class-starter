56d327a8532840d74db027602de52e7a27c56a9c
AntMan <xanthonyalvarezx@gmail.com>
2020-07-06 15:18:50 -0400

    initial commit

---
+++ assets/scripts/coin.js
@@ -0,0 +1,20 @@
+class Coin {
+    constructor(initialState) {
+       
+        this.state = 0
+
+        
+            
+    }
+flip(){
+    console.log("coin.flip() has been invoked.")
+}
+    
+toString(){
+    console.log("coin.toString() has been invoked.")
+}
+toHTML(){
+    console.log("coin.toHTML() has been invoked.")
+}
+
+}
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,20 +1,26 @@
+const coin1 = new Coin(1)
+const coin2 = new Coin(0)
+const coin3 = new Coin(1)
+const coin4 = new Coin(0)
+
 const coinWithoutClass = {
-
+    
+    
     state: 0,
-
+    
     flip: function () {
         console.log("coin.flip() has been invoked.")
     },
-
+    
     toString: function () {
         console.log("coin.toString() has been invoked.")
     },
-
+    
     toHTML: function () {
         let image = document.createElement('img')
-
+        
         console.log("coin.toHTML() has been invoked.")
-
+        
         return image
     }
 
